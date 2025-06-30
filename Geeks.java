683cdb40f45109958455330631cbcb349a5620bb
sowmya <sowmya.s@ritchennai.edu.in>
2025-06-30 14:57:39 +0530

    Generic class

---
+++ Geeks.java
@@ -0,0 +1,34 @@
+package one;
+
+class Test<T, U>
+	{
+	    T obj1;  // An object of type T
+	    U obj2;  // An object of type U
+
+	    // constructor
+	    Test(T obj1, U obj2)
+	    {
+	        this.obj1 = obj1;
+	        this.obj2 = obj2;
+	    }
+
+	    // To print objects of T and U
+	    public void print()
+	    {
+	        System.out.println(obj1);
+	        System.out.println(obj2);
+	    }
+	}
+	class Geeks
+	{
+	    public static void main (String[] args)
+	    {
+	        Test <String, Integer> obj =
+	            new Test<String, Integer>("GfG", 15);
+
+	        obj.print();
+	    }
+	
+	}
+
+
